789a860af44f07317323fa24e8883a9d1483da5c
digvijay315 <digvijaykumar.315@gmail.com>
2026-08-10 17:00:13 +0530

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
-  // baseURL:"https://saas-billing-app.onrender.com/"
+  // baseURL: "http://localhost:5000/",
+  baseURL:"https://zoox.onrender.com/"
 });
 
 let activeRequests = 0;
